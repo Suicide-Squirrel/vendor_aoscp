b734dcc97400d37c9de5beb07a7b64cd815bb18c
pavlaras <theodoroupaulos@gmail.com>
2017-11-22 23:05:07 +0100

    Add GoogleCamera as default camera app

--- configs/common.mk
+++ configs/common.mk
@@ -92,6 +92,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aoscp/prebuilt/MiXplorer/MiXplorer.apk:system/app/MiXplorer/MiXplorer.apk
 
+# GoogleCamera
+PRODUCT_COPY_FILES += \
+    vendor/aoscp/prebuilt/GoogleCamera/GoogleCamera.apk:system/app/GoogleCamera/GoogleCamera.apk
+
 #SELinux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
